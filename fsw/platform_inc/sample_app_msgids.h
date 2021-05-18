3f6f6fa53d19330d86fc4ac915a87c43ccabb788
Andrew Harris <andrew.harris@jhuapl.edu>
2021-05-18 14:16:00 -0400

    #2: Update MIDS to use global MID base

--- fsw/platform_inc/sample_app_msgids.h
+++ fsw/platform_inc/sample_app_msgids.h
@@ -20,21 +20,14 @@
 **
 *************************************************************************/
 
-/**
- * @file
- *
- * Define Sample App Message IDs
- *
- * \note The Sample App assumes default configuration which uses V1 of message id implementation
- */
-
 #ifndef SAMPLE_APP_MSGIDS_H
 #define SAMPLE_APP_MSGIDS_H
 
-/* V1 Command Message IDs must be 0x18xx */
-#define SAMPLE_APP_CMD_MID     0x1882
-#define SAMPLE_APP_SEND_HK_MID 0x1883
-/* V1 Telemetry Message IDs must be 0x08xx */
-#define SAMPLE_APP_HK_TLM_MID 0x0883
+#include "cfe_msgids.h"
+
+#define SAMPLE_APP_CMD_MID     (CFE_PLATFORM_CMD_MID_BASE + 0x82)
+#define SAMPLE_APP_SEND_HK_MID (CFE_PLATFORM_CMD_MID_BASE + 0x83)
+
+#define SAMPLE_APP_HK_TLM_MID  (CFE_PLATFORM_TLM_MID_BASE + 0x83)
 
 #endif /* SAMPLE_APP_MSGIDS_H */
